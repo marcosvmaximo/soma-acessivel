7aa4a1ce9c1ff317047de4b009574fecc3707c19
Marcos Maximo <marcosvinmaximo@gmail.com>
2025-04-23 23:22:45 -0300

    Initial Commit

---
+++ AcessivelSoma/AcessivelSomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcessivelSomaApp.swift
+//  AcessivelSoma
+//
+//  Created by Marcos Máximo on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct AcessivelSomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AcessivelSomaTests/AcessivelSomaTests.swift
@@ -0,0 +1,17 @@
+//
+//  AcessivelSomaTests.swift
+//  AcessivelSomaTests
+//
+//  Created by Marcos Máximo on 23/04/25.
+//
+
+import Testing
+@testable import AcessivelSoma
+
+struct AcessivelSomaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
